8f0c2cdf6860f3f7e94c4d0391d030fd82160ff2
Alisher <alishermail111@gmail.com>
2024-02-25 10:14:30 +0600

    created temperature entity

---
+++ src/main/java/com/example/demo/model/Temperature.java
@@ -0,0 +1,30 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Temperature {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private double value;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
